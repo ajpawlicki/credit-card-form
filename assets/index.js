901656693aecdbb171c71985272f67529213b348
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-09-25 22:19:37 -0700

    Create function to only type numbers in input

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,31 @@
+$(document).ready(() => {
+  const name = $('#name');
+  const cardNumber = $('#card-number');
+  const cvv = $('#cvv2');
+
+  const month = $('#month');
+  const year = $('#year');
+  
+  const submit = $('#submit');
+
+  submit.click(e => {
+    e.preventDefault();
+
+    if (!isValidName(name.val())) {
+      alert('Invalid name!');
+    } else if (!isValidCardNumber(cardNumber.val(), cvv.val())) {
+      alert('Invalid card number and cvv!');
+    } else if (!isValidExpiration(month.val(), year.val())) {
+      alert('Invalid expiration!');
+    } else {
+      alert('Great!');
+    }
+  });
+
+  cardNumber.keydown(typeNumsOnly);
+  cvv.keydown(typeNumsOnly);
+});
+
 const isValidName = (name) => {
   return name.trim().length > 4;
 };
@@ -35,36 +63,20 @@ const isValidExpiration = (month, year) => {
     return true;
   } else if (Number(year) === currYear) {
     return month > currMonth;
-  } else {
-    return false;
   }
-};
-
-const typeNumsOnly = (event) => {
-
-};
-
-$(document).ready(() => {
-  const name = $('#name');
-  const cardNumber = $('#card-number');
-  const cvv = $('#cvv2');
-
-  const month = $('#month');
-  const year = $('#year');
   
-  const submit = $('#submit');
+  return false;
+};
 
-  submit.click(e => {
-    e.preventDefault();
+const typeNumsOnly = (e) => {
+  const isValidKey = $.inArray(e.keyCode, [46, 8, 9, 27, 13]) !== -1;
+  const isSelectAll = e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true);
+  const isDirectionKey = e.keyCode >= 35 && e.keyCode <= 40;
+  
+  if (isValidKey || isSelectAll || isDirectionKey) return;
+  
+  const isNotANumber = e.shiftKey || (e.keyCode < 48 || e.keyCode > 57);
+  const isNotOnNumberPad = e.keyCode < 96 || e.keyCode > 105;
 
-    if (!isValidName(name.val())) {
-      alert('Invalid name!');
-    } else if (!isValidCardNumber(cardNumber.val(), cvv.val())) {
-      alert('Invalid card number and cvv!');
-    } else if (!isValidExpiration(month.val(), year.val())) {
-      alert('Invalid expiration!');
-    } else {
-      alert('Great!');
-    }
-  });
-});
+  if (isNotANumber && isNotOnNumberPad) e.preventDefault();
+};
