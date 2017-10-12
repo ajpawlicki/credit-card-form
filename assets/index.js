e39d493186122d6b220ce1b4248dddea93a4f17a
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-10-12 14:12:22 -0700

    Abstract conditions into functions

--- assets/index.js
+++ assets/index.js
@@ -48,19 +48,14 @@ $(document).ready(() => {
     cardNumberEl.maxLength = '16';
     cvvEl.maxLength = '4';
 
-    const startsWith4 = cardNumber.val()[0] === '4';
-    
-    if (startsWith4) {
+    if (startsWith4(cardNumber.val())) {
       amex.addClass('transparent');
       
       cardNumberEl.maxLength = '16';
       cvvEl.maxLength = '3';
     }
-    
-    const firstTwoChars = cardNumber.val().slice(0,2);
-    const hasAmexFirstTwoChars = firstTwoChars === '34' || firstTwoChars === '37';
 
-    if (hasAmexFirstTwoChars) {
+    if (hasAmexFirstTwoChars(cardNumber.val())) {
       visa.addClass('transparent');
 
       cardNumberEl.maxLength = '15';
@@ -100,24 +95,30 @@ const isValidCardNumber = (num, cvv) => {
   return isValidVisa || isValidAmEx;
 };
 
+const startsWith4 = (num) => {
+  return num[0] === '4';
+};
+
 const isValidVisaNum = (num) => {
   const isValidLength = num.length === 16;
-  const startsWith4 = num[0] === '4';
   
-  return isValidLength && startsWith4;
+  return isValidLength && startsWith4(num);
 };
 
 const isValidVisaCVV = (cvv) => {
   return cvv.length === 3;
 };
 
+const hasAmexFirstTwoChars = (num) => {
+  const firstTwoChars = num.slice(0,2);
+
+  return firstTwoChars === '34' || firstTwoChars === '37';
+};
+
 const isValidAmExNum = (num) => {
   const isValidLength = num.length === 15;
 
-  const firstTwoChars = num.slice(0,2);
-  const hasCorrectFirstTwoChars = firstTwoChars === '34' || firstTwoChars === '37';
-  
-  return isValidLength && hasCorrectFirstTwoChars;
+  return isValidLength && hasAmexFirstTwoChars(num);
 };
 
 const isValidAmExCVV = (cvv) => {
@@ -142,9 +143,10 @@ const isValidExpiration = (month, year) => {
 const typeNumsOnly = (e) => {
   const isValidKey = $.inArray(e.keyCode, [46, 8, 9, 27, 13]) !== -1;
   const isSelectAll = e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true);
+  const isReloadPage = e.keyCode === 82 && (e.ctrlKey === true || e.metaKey === true);
   const isDirectionKey = e.keyCode >= 35 && e.keyCode <= 40;
   
-  if (isValidKey || isSelectAll || isDirectionKey) return;
+  if (isValidKey || isSelectAll || isReloadPage || isDirectionKey) return;
   
   const isNotANumber = e.shiftKey || (e.keyCode < 48 || e.keyCode > 57);
   const isNotOnNumberPad = e.keyCode < 96 || e.keyCode > 105;
